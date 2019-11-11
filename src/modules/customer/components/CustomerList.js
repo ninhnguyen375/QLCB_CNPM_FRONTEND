a6a708a8402455e572a44121e3df2e8635af6098
Ninh <ninhnguyen375@gmail.com>
2019-11-11 23:56:11 +0700

    init Customer manager

---
+++ src/modules/customer/components/CustomerList.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react'
+import {
+  Table,
+  Card,
+  Button,
+  Row,
+  Col,
+  Input,
+  Icon,
+  notification,
+  Tag,
+} from 'antd'
+import Modal from '../../../common/components/widgets/Modal'
+import { handleError } from '../../../common/utils/handleError'
+import removeNullObject from '../../../common/utils/removeObjectNull'
+import { Link } from 'react-router-dom'
+
+export class CustomerList extends Component {
+  state = {
+    pagination: {},
+    search: {},
+  }
+
+  columns = [
+    {
+      key: 'avatar',
+      align: 'right',
+      render: () => (
+        <img
+          src={require('../../../assets/images/user.svg')}
+          alt='avatar'
+          width={35}
+        />
+      ),
+    },
+    {
+      key: 'FullName',
+      dataIndex: 'fullName',
+      title: ' Họ tên ',
+      sorter: true,
+      render: (value, record) => (
+        <Link to={`/admin/customer/${record.id}`}>{value}</Link>
+      ),
+    },
+    {
+      key: 'Phone',
+      dataIndex: 'phone',
+      title: 'Số điện thoại',
+      sorter: true,
+      render: (value, record) => (
+        <div>
+          <Icon type='phone' /> {value}
+        </div>
+      ),
+    },
+    {
+      key: 'BookingCount',
+      dataIndex: 'bookingCount',
+      title: 'Số lần đặt',
+      sorter: true,
+      align: 'center',
+      render: (value, record) => <Tag color='blue'>{value}</Tag>,
+    },
+    {
+      key: 'action',
+      title: ' Thao tác ',
+      align: 'right',
+      render: r => {
+        return (
+          <div className='d-flex justify-content-end'>
+            <Link to={`/admin/customer/${r.id}`}>
+              <Button type='primary' icon='info-circle'>
+                Chi tiết
+              </Button>
+            </Link>
+          </div>
+        )
+      },
+    },
+  ]
+
+  handleChangeTable = async ({ current }, filter, sorter) => {
+    const { columnKey, order } = sorter
+    const sortParam =
+      order === 'ascend' ? { sortAsc: columnKey } : { sortDesc: columnKey }
+
+    columnKey
+      ? await this.getCustomers(current, null, sortParam)
+      : await this.getCustomers(current)
+  }
+
+  getCustomers = async (current, pageSize, params) => {
+    const { getCustomers } = this.props
+    let { search } = this.state
+
+    search = removeNullObject(search)
+    const res = await getCustomers(current, pageSize, { ...search, ...params })
+    if (res.success) {
+      this.setState({ pagination: res })
+    } else {
+      handleError(res, null, notification)
+    }
+  }
+
+  async componentDidMount() {
+    await this.getCustomers()
+  }
+
+  handleSearch = name => value => {
+    this.setState({ search: { ...this.state.search, [name]: value } }, () => {
+      this.getCustomers()
+      console.log(this.state)
+    })
+  }
+
+  hanleChangeSearch = e => {
+    const { name, value } = e.target
+    this.setState({ search: { ...this.state.search, [name]: value } })
+  }
+
+  handleReset = () => {
+    this.setState({ search: {} }, () => this.getCustomers())
+  }
+
+  render() {
+    const { pagination, search = {} } = this.state
+
+    let { customers } = this.props
+    customers = customers || []
+
+    return (
+      <Card title={<b>Khách Hàng</b>}>
+        <Row type='flex' justify='space-between'>
+          <Col>
+            <div className='d-flex'>
+              <Input.Search
+                value={search.name}
+                style={{ marginRight: 5, width: 250 }}
+                name='name'
+                onSearch={this.handleSearch('name')}
+                placeholder='Tìm theo Tên khách hàng'
+                onChange={this.hanleChangeSearch}
+              ></Input.Search>
+            </div>
+          </Col>
+          <Col>
+            <Button onClick={this.handleReset} icon='sync'>
+              Làm mới
+            </Button>
+          </Col>
+        </Row>
+        <br />
+
+        <Table
+          pagination={pagination}
+          onChange={this.handleChangeTable}
+          columns={this.columns}
+          rowKey={i => i.id}
+          dataSource={customers || []}
+        ></Table>
+      </Card>
+    )
+  }
+}
+
+export default CustomerList
---
+++ src/modules/customer/handlers.js
@@ -0,0 +1,59 @@
+import { fetchAuthLoading } from '../../common/effects'
+import { ENDPOINTS, LIMIT } from './models'
+import { setCustomers } from './actions'
+
+export async function getCustomersAsync(
+  current = 1,
+  pageSize = LIMIT,
+  params = {},
+) {
+  const res = await fetchAuthLoading({
+    url: ENDPOINTS.getCustomers,
+    method: 'GET',
+    params: {
+      current,
+      pageSize,
+      ...params,
+    },
+  })
+  return res.data
+}
+
+export const getCustomerAsync = async id => {
+  const result = await fetchAuthLoading({
+    url: ENDPOINTS.getCustomer(id),
+    method: 'GET',
+  })
+  return result.data
+}
+
+export const updateCustomer = async (data, id) => {
+  const result = await fetchAuthLoading({
+    url: ENDPOINTS.updateCustomer(id),
+    method: 'PUT',
+    data,
+  })
+  return result.data
+}
+
+export async function createCustomerAsync(customer = {}) {
+  const res = await fetchAuthLoading({
+    url: ENDPOINTS.createCustomer,
+    method: 'POST',
+    data: customer,
+  })
+  return res.data
+}
+
+export default (dispatch, props) => ({
+  getCustomers: async (current, pageSize, params) => {
+    try {
+      const res = await getCustomersAsync(current, pageSize, params)
+      dispatch(setCustomers(res))
+      return { ...res, success: true }
+    } catch (err) {
+      dispatch(setCustomers({}))
+      return { ...err, success: false }
+    }
+  },
+})
